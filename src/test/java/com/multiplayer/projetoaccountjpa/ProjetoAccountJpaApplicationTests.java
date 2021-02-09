91d4d4e59d12671741e3868d97a6223b120a888c
Danilo Elias <daniloselias@gmail.com>
2021-02-09 13:12:43 -0300

    Estrutura inicial do projeto usando Spring Boot e Spring Data JPA

---
+++ src/test/java/com/multiplayer/projetoaccountjpa/ProjetoAccountJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiplayer.projetoaccountjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAccountJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
